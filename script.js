943bda3224a04ac3b69aed9de193bb715e120828
anandkp6937 <anandkp6937@gmail.com>
2022-11-16 21:38:15 +0530

    added js file

---
+++ script.js
@@ -0,0 +1,9 @@
+const likebtn=document.getElementById("like");
+let count=0;
+likebtn.addEventListener("click",()=>{
+
+    likebtn.style.background="#b33071";
+    likebtn.style.color="#ffffff";
+    count+1;
+
+})
