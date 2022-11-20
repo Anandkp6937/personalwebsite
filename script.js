ba8e760462585a71c5faaeda0c3d68c496da7fe3
anandkp6937 <anandkp6937@gmail.com>
2022-11-20 15:35:38 +0530

    added new picture blueimg and profile2

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const likebtn=document.getElementById("like");
+const sendbtn=document.getElementById("send");
+
 let count=0;
-likebtn.addEventListener("click",()=>{
-
-    likebtn.style.background="#b33071";
-    likebtn.style.color="#ffffff";
-    count+1;
-
+ likebtn.addEventListener("click",function(){
+   if(count==0){
+     likebtn.style.background="#400c84";
+     likebtn.style.color="#ffffff";
+     count=1;
+   }
+   else{
+     likebtn.style.background="#ffffff";
+     likebtn.style.color="#000000";
+     count=0;
+   }
+ })
+sendbtn.addEventListener("click",function(){
+  console.log("sended");
 })
